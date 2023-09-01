417eb79382eec6f243b12392895684c264129360
Soubhik Chakraborty <58596900+soubhikc500@users.noreply.github.com>
2023-09-01 19:48:16 +0530

    Add files via upload

---
+++ Assignment Let's Upgrade.py
@@ -0,0 +1,12 @@
+#Assignment By Soubhik Chakraborty
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+print("The floor division is ", a/b)
+
+print(f"{a} power {b} is", a**b)
+
+if(a == b):
+    print("True")
+else:
+    print("False")
